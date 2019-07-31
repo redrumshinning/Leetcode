5ded4c79f567639042bcf6eb71ece910f54a5af9
redrumshinning <1184761736@qq.com>
2019-07-31 17:34:41 +0800

    add

--- 215.Kth Largest Element in an Array.py
+++ 215.Kth Largest Element in an Array.py
@@ -18,27 +18,4 @@ class Solution(object):
         return -res
 
 
-class Quantity:
-    def __init__(self,storage_name):
-        self.storage_name = storage_name
-    def __set__(self,instance,value):
-        if value > 0:
-            instance.__dict__[self.storage_name] = value
-        else:
-            raise ValueError('value must be > 0')
-class LineItem:
-    weight = Quantity('weight')
-    price = Quantity('price')
-
-    def __init__(self,discription,weight,price):
-        self.discription = discription
-        self.weight = weight
-        self.price = price
-
-    def subtotal(self):
-        return self.weight * self.price
-
-truffle = LineItem('White truffle', 100, 1)
-print(truffle.price)
-
 
